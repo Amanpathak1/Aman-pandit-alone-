8ef7f99833d277bf2928a9e0a9590911667dd3c1
Aman Pathak <155675919+Amanpathak1@users.noreply.github.com>
2024-05-03 11:33:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 api_id = "22253680"
 api_hash = "4fc4c646519fe43891bebd1449744b7e"
 bot_token = "7050029715:AAHbEX1D80bQN6kzd1zogJHl2x20rkRjNGg"
-auth_groups = "-1002132958585"
+auth_groups = "-1004140694328"
